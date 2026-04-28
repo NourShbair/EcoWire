949f16a9fa8caa4d8e24d757d738e4353918fb36
Nour <nshbair@gmail.com>
2026-04-28 19:01:42 +0100

    Setup spring-boot project

---
+++ Backend/ecowire/src/main/java/com/ecowire/ecowire/EcowireApplication.java
@@ -0,0 +1,17 @@
+package com.ecowire.ecowire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcowireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcowireApplication.class, args);
+		System.out.println("Nourrrr is hereeee");
+
+
+
+	}
+
+}
---
+++ Backend/ecowire/src/test/java/com/ecowire/ecowire/EcowireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecowire.ecowire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcowireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
